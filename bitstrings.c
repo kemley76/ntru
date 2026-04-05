1c0d4a2d5564e760f1dbcb8f763521ea5bef5317
kemley76 <kaden.emley@gmail.com>
2026-04-05 00:46:44 -0400

    add bitstring splitting
    Signed-off-by: kemley76 <kaden.emley@gmail.com>

--- bitstrings.c
+++ bitstrings.c
@@ -1,6 +1,7 @@
 #include "bitstrings.h"
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 // n is the number of bits
 bitstring_t new_bistring(size_t n) {
@@ -10,4 +11,17 @@ bitstring_t new_bistring(size_t n) {
 
 uint8_t get_nth_bit(bitstring_t bits, size_t n) {
     return (bits.data[n / 8] >> n % 8) & 0x1;
+}
+
+// splits a bitstring of n + m bits in one n of bits and one of m bits
+// it is assumed that the split occurs on a byte boundary (n % 8 == 0)
+bitstring_pair_t split_at(bitstring_t original, size_t n) {
+    // we can point to the same data in the original because it splits on a byte
+    // boundary. This assumes that the original won't get freed until we're done
+    // with these
+    bitstring_t front_half = (bitstring_t){.data = original.data, .length = n};
+    bitstring_t back_half = new_bistring(original.length - n);
+    back_half.data = original.data + (n / 8);
+
+    return (bitstring_pair_t){front_half, back_half};
 }
--- bitstrings.h
+++ bitstrings.h
@@ -1,3 +1,6 @@
+#ifndef BITSTRINGS_H
+#define BITSTRINGS_H
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -6,6 +9,15 @@ typedef struct {
     size_t length; // number of bits in the bitstring
 } bitstring_t;
 
+typedef struct {
+    bitstring_t front;
+    bitstring_t back;
+} bitstring_pair_t;
+
 bitstring_t new_bistring(size_t n);
 
 uint8_t get_nth_bit(bitstring_t bits, size_t n);
+
+bitstring_pair_t split_at(bitstring_t original, size_t n);
+
+#endif
