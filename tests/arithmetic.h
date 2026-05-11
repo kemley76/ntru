66b65a11397cc9b36d343ffd89ee0d5962b2202f
kemley76 <kaden.emley@gmail.com>
2026-05-11 17:22:36 -0400

    create tests for inverse functions
    Signed-off-by: kemley76 <kaden.emley@gmail.com>

--- tests/arithmetic.h
+++ tests/arithmetic.h
@@ -3,4 +3,9 @@
 int test_all_arithmetic();
 int test_poly_mul_1();
 int test_poly_inv_s3();
+int test_2_poly_inv_s3();
+int test_poly_inv_s2();
+int test_2_poly_inv_s2();
+int test_poly_inv_sq();
+int test_2_poly_inv_sq();
 void print_poly(const char *name, poly *a);
